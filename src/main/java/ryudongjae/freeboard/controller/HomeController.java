f02c16bdf770227ee115f25e2979420f8ca06421
ryudongjae <rdj10149@gmail.com>
2021-04-16 15:52:06 +0900

    Controller

---
+++ src/main/java/ryudongjae/freeboard/controller/HomeController.java
@@ -0,0 +1,49 @@
+package ryudongjae.freeboard.controller;
+
+import com.freeboard01.api.user.UserForm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ryudongjae.freeboard.domain.user.UserService;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    private UserService userService;
+
+    @Autowired
+    public HomeController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/board")
+    public String board(HttpSession httpSession, Model model) {
+        UserForm loginUser = (UserForm) httpSession.getAttribute("USER");
+        if(loginUser != null ) {
+            model.addAttribute("accountId", loginUser.getAccountId());
+            model.addAttribute("accountRole" , userService.findUserRole(loginUser));
+        }
+        return "board";
+    }
+
+    @GetMapping("/join")
+    public String join() { return "join"; }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.removeAttribute("USER");
+        return "index";
+    }
+
+}
